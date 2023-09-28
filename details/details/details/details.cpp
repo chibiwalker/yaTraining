be7b73bead845875f312d44c18702bb117f893f3
chibiwalker <chibiwalker@bk.ru>
2023-09-28 15:19:24 +0300

    upload solves, that passed all tests

---
+++ details/details/details/details.cpp
@@ -0,0 +1,26 @@
+﻿// details.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+
+int main()
+{
+    int N, K, M, res, Z,det,ostZ,oldN;
+    std::cin >> N >> K >> M; 
+    oldN = N;
+    det = K / M;
+    ostZ = K % M;
+    res = 0;
+    if (M > K)
+        std::cout << 0;
+    else
+    {
+        while (N / K > 0)
+        {
+            Z = N / K;
+            res += Z * det;
+            N = N % K + Z * ostZ;
+        }
+        std::cout << res;
+    }
+}
---
+++ underground/underground/underground.cpp
@@ -0,0 +1,24 @@
+﻿// underground.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+
+int main()
+{
+    int a, b, n, m,aMin,bMin,aMax,bMax;
+    std::cin >> a;
+    std::cin >> b;
+    std::cin >> n;
+    std::cin >> m;
+    aMin = a * (n-1) + n;
+    bMin = b * (m-1) + m;
+    aMax = aMin + 2 * a;
+    bMax = bMin + 2 * b;
+    if (aMin > bMin + 2 * b || bMin > aMin + 2 * a)
+        std::cout << -1;
+    else
+    {
+        std::cout << std::max(aMin, bMin) <<" "<< std::min(aMax, bMax);
+    }
+}
+
